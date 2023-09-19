6dc343f9356c45df15bab5dffed15311f9beef7c
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-09-19 19:55:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	var firstname = document.getElementById("fname").value;
+	var lastname = document.getElementById("lname").value;
+  alert(firstname + " " + lastname);
 }
